773aadd26569c4878a026185e694347dc1e27587
Maria gabriela <maria.soares.99@aluno.edu.pi.gov.br>
2025-09-05 10:04:38 -0300

    Add files via upload

---
+++ MeuPrimeiroProjeto/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Atividade {
+    public static void main(String[] args) {
+
+        double fan =22;
+        double celsius = (5.0/9.0)*(fan-32);
+
+        System.out.println();"temperatura em celsius": + celsius);
+
+    }
+}
